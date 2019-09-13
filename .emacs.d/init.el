37bd8f06c5a5a90efec5e98ddd597a1a1a0f8f1f
Abraham Raji <abrahamraji99@gmail.com>
2019-09-13 15:13:50 +0530

    smaller init

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,15 +1,1 @@
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (eshell-fringe-status yasnippet-snippets writegood-mode web-mode use-package try sx smex smartparens rainbow-mode rainbow-delimiters projectile ox-twbs ox-reveal org-bullets multiple-cursors magit lorem-ipsum linum-relative kivy-mode hydra htmlize gruvbox-theme git-timemachine git-gutter ggtags flycheck-clang-analyzer flx expand-region emmet-mode elpy doom-themes doom-modeline dired-open dired-hide-dotfiles dashboard counsel blacken beacon anaconda-mode ac-c-headers))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
